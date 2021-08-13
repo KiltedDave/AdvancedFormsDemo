dcd61cf14074b19de13beec3a4e7e26a17ebaf94
dpm4891 <dpm4891@yahoo.com>
2021-08-12 22:40:01 -0500

    Added sample form

---
+++ BlazorApp/Models/SampleModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class SampleModel
+    {
+        public int SampleInt { get; set; }
+        public double SampleDouble { get; set; }
+        public bool SampleBool { get; set; }
+        public DateTime SampleDate { get; set; }
+        public string DropDownValue { get; set; }
+        public string SamplePassword { get; set; }
+    }
+}

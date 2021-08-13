e47f98a5c4ab5714159d17c38edf0b5f564b1541
dpm4891 <dpm4891@yahoo.com>
2021-08-12 23:01:53 -0500

    Added cascading form page

---
+++ BlazorApp/Models/CascadeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class CascadeModel
+    {
+        public int FirstId { get; set; }
+        public int SecondId { get; set; }
+    }
+}
